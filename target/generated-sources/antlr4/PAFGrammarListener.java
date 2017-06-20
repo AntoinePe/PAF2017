3b14b69d6669604b9dd136a15bd4cddede89f287
Antoine Pelletier <antoine.pelletier@telecom-paristech.fr>
2017-06-20 09:50:24 +0200

    h

--- target/generated-sources/antlr4/PAFGrammarListener.java
+++ target/generated-sources/antlr4/PAFGrammarListener.java
@@ -1,5 +1,4 @@
-// Generated from PAFGrammar.g4 by ANTLR 4.4
-import org.antlr.v4.runtime.misc.NotNull;
+// Generated from PAFGrammar.g4 by ANTLR 4.7
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 /**
@@ -8,13 +7,123 @@ import org.antlr.v4.runtime.tree.ParseTreeListener;
  */
 public interface PAFGrammarListener extends ParseTreeListener {
 	/**
-	 * Enter a parse tree produced by {@link PAFGrammarParser#s}.
+	 * Enter a parse tree produced by {@link PAFGrammarParser#instructions}.
 	 * @param ctx the parse tree
 	 */
-	void enterS(@NotNull PAFGrammarParser.SContext ctx);
+	void enterInstructions(PAFGrammarParser.InstructionsContext ctx);
 	/**
-	 * Exit a parse tree produced by {@link PAFGrammarParser#s}.
+	 * Exit a parse tree produced by {@link PAFGrammarParser#instructions}.
 	 * @param ctx the parse tree
 	 */
-	void exitS(@NotNull PAFGrammarParser.SContext ctx);
+	void exitInstructions(PAFGrammarParser.InstructionsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void enterInstruction(PAFGrammarParser.InstructionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void exitInstruction(PAFGrammarParser.InstructionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#operation1}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperation1(PAFGrammarParser.Operation1Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#operation1}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperation1(PAFGrammarParser.Operation1Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#operation2}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperation2(PAFGrammarParser.Operation2Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#operation2}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperation2(PAFGrammarParser.Operation2Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#term}.
+	 * @param ctx the parse tree
+	 */
+	void enterTerm(PAFGrammarParser.TermContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#term}.
+	 * @param ctx the parse tree
+	 */
+	void exitTerm(PAFGrammarParser.TermContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#if_condition}.
+	 * @param ctx the parse tree
+	 */
+	void enterIf_condition(PAFGrammarParser.If_conditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#if_condition}.
+	 * @param ctx the parse tree
+	 */
+	void exitIf_condition(PAFGrammarParser.If_conditionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#else_condition}.
+	 * @param ctx the parse tree
+	 */
+	void enterElse_condition(PAFGrammarParser.Else_conditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#else_condition}.
+	 * @param ctx the parse tree
+	 */
+	void exitElse_condition(PAFGrammarParser.Else_conditionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#while_loop}.
+	 * @param ctx the parse tree
+	 */
+	void enterWhile_loop(PAFGrammarParser.While_loopContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#while_loop}.
+	 * @param ctx the parse tree
+	 */
+	void exitWhile_loop(PAFGrammarParser.While_loopContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#dowhile_loop}.
+	 * @param ctx the parse tree
+	 */
+	void enterDowhile_loop(PAFGrammarParser.Dowhile_loopContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#dowhile_loop}.
+	 * @param ctx the parse tree
+	 */
+	void exitDowhile_loop(PAFGrammarParser.Dowhile_loopContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#for_loop}.
+	 * @param ctx the parse tree
+	 */
+	void enterFor_loop(PAFGrammarParser.For_loopContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#for_loop}.
+	 * @param ctx the parse tree
+	 */
+	void exitFor_loop(PAFGrammarParser.For_loopContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#assigning}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssigning(PAFGrammarParser.AssigningContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#assigning}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssigning(PAFGrammarParser.AssigningContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PAFGrammarParser#condition}.
+	 * @param ctx the parse tree
+	 */
+	void enterCondition(PAFGrammarParser.ConditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PAFGrammarParser#condition}.
+	 * @param ctx the parse tree
+	 */
+	void exitCondition(PAFGrammarParser.ConditionContext ctx);
 }
